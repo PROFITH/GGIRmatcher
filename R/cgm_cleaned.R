14d3c3385908e5a0db6dd3087247c001eabe7c5b
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2024-10-25 17:11:13 +0200

    data: clean version of the raw cgm file in extdata

---
+++ R/cgm_cleaned.R
@@ -0,0 +1,12 @@
+#' Cleaned CGM Example File
+#'
+#' Cleaned version of the inst/extdata/cgm_raw_sample.csv
+#'
+#' @format ## `cgm_cleaned`
+#' A data frame with 1,339 rows and 2 columns:
+#' \describe{
+#'   \item{timestamp}{Character containing time stamp in ISO8601 format}
+#'   \item{GLUC}{Numeric. Glucose in mg/dL}
+#' }
+#' @source TEMPUS project
+"cgm_cleaned"
