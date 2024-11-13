f864cc88c4baef30a569e19ef78a6decb2647ac2
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2024-11-13 09:28:31 +0100

    cleaned code for create_folder_structure, fixed return as list, and safe check for defined outputdir

--- R/create_folder_structure.R
+++ R/create_folder_structure.R
@@ -16,22 +16,22 @@ create_folder_structure = function(outputdir,
                                    GGIR_dirname = "GGIR", 
                                    additional_dirname = "CGM",
                                    matched_dirname = "GGIRmatcher") {
+  # safe check that outputdir exists
+  if (!dir.exists(outputdir)) stop("Your defined output directory does not exist, please provide path to a existing (ideally empty) folder")
   # folder names
   GGIR_output_folder = file.path(outputdir, GGIR_dirname)
   matched_output_folder = file.path(outputdir, matched_dirname)
   additional_output_folder = file.path(outputdir, additional_dirname)
   # create directories
-  suppressWarnings({
-    dir.create(GGIR_output_folder)
-    dir.create(additional_output_folder)
-    dir.create(matched_output_folder)
-  })
+  if (!dir.exists(GGIR_output_folder)) dir.create(GGIR_output_folder)
+  if (!dir.exists(additional_output_folder)) dir.create(additional_output_folder)
+  if (!dir.exists(matched_output_folder)) dir.create(matched_output_folder)
   # info messages
   cat(paste("\nThe GGIR output folder is", GGIR_output_folder))
   cat(paste("\nThe additional sensor output folder is", additional_output_folder))
   cat(paste("\nThe matched output folder is", matched_output_folder))
   # return directories
-  return(GGIR_ouputdir = GGIR_output_folder,
-         additional_outputdir = additional_output_folder,
-         GGIRmatcher_outputdir = matched_output_folder)
+  return(list(GGIR_ouputdir = GGIR_output_folder,
+              additional_outputdir = additional_output_folder,
+              GGIRmatcher_outputdir = matched_output_folder))
 }
